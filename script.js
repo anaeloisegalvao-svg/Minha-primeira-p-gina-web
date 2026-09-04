76043459778174076d1a0325288fbb01cc031970
anaeloisegalvao-svg <ana.eloise.galvao@escola.pr.gov.br>
2026-09-04 10:36:45 -0300

    gf

--- script.js
+++ script.js
@@ -2,28 +2,23 @@ const botoes = document.querySelectorAll("button");
 
 botoes.forEach(function (botao) {
    let curtiu = false;
-   botao.addEventListener("click", botaoClicado);
-   function botaoClicado() {
+   
+   botao.addEventListener("click", function () {
       let texto = botao.querySelector("span");
-      if (curtiu === false){
-        texto.textContent++;
-        curtiu = true;
-      } else{ 
-         texto.textContent--;
+      if (!curtiu) {
+         texto.textContent = Number(texto.textContent) + 1;
+         curtiu = true;
+      } else { 
+         texto.textContent = Number(texto.textContent) - 1;
          curtiu = false;
       }
-   } 
-})
+   });
+});
     
 const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
 
-  btnTemaEscuro.addEventListener("click", mudaTema);
-
-function mudaTema() {
-   const corpoPagina = document.body;
-   if(corpoPagina.classList.contains("tema-escuro")) {
-      corpoPagina.classList.remove("tema-escuro");
-   } else{
-      corpoPagina.classList.add("tema-escuro");
-   }
+if (btnTemaEscuro) {
+   btnTemaEscuro.addEventListener("click", function () {
+      document.body.classList.toggle("tema-escuro");
+   });
 }
